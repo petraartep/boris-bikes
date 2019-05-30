a1b6dedb78d2754bbabb8edce7d2d97d7ee5df6d
petraartep <petra.pogacsas@gmail.com>
2019-05-30 16:06:10 +0100

    test for docked bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,5 +10,20 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns docked bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
 
 end
