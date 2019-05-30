0884ada6c3decbda6a278a4296ebb963ef5d6f0f
petraartep <petra.pogacsas@gmail.com>
2019-05-30 22:35:37 +0100

    update:

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,10 @@
 require_relative 'bike'
 
  class DockingStation
-  attr_reader :bike 
+    attr_reader :bike
 
     def release_bike
-        Bike.new
+      Bike.new
     end
 
     def dock(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,29 +1,25 @@
 require 'docking_station'
 
 describe DockingStation do
-#    it 'responds to release_bike' do
-#    expect(subject).to respond_to :release_bike
+    
   it { is_expected.to respond_to :release_bike }
-  
+
   it 'releases working bikes' do
     bike = subject.release_bike
-    expect(bike).to be_working
+    expect(bike).to be_working 
   end
 
-  it { is_expected.to respond_to(:dock).with(1).argument }
+   it { is_expected.to respond_to(:dock).with(1).argument }
+   it { is_expected.to respond_to(:bike) }
 
-  it { is_expected.to respond_to(:bike) }
-
-  it 'docks something' do
+   it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
-  end
+   end
 
-  it 'returns docked bike' do
+   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
-  end
-
-
+   end
 end
